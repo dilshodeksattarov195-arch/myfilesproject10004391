61396995e5493d7556dbfc32ed605e03ef26be61
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-28 17:37:05 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class clusterCtringifyWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = clusterCtringifyWorker()
+    print("Worker engine initialized.")
